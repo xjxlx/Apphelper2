cc7096e47bc0eff2c579730190fa0785685ab2ab
xjxliuxing <xjxliuxing@163.com>
2023-05-07 21:02:39 +0800

    update  ResourcesUtil.kt

--- src/main/java/com/android/apphelper2/utils/ResourcesUtil.kt
+++ src/main/java/com/android/apphelper2/utils/ResourcesUtil.kt
@@ -5,6 +5,9 @@ import androidx.annotation.StringRes
 
 object ResourcesUtil {
 
+    /**
+     * Returns the res ---> string content
+     */
     fun getString(context: Context, @StringRes id: Int): String {
         kotlin.runCatching {
             context.resources.getString(id)
@@ -15,6 +18,25 @@ object ResourcesUtil {
             .onFailure {
                 return ""
             }
+        return ""
+    }
+
+    fun getStringForReflect(context: Context, id: String): String {
+        runCatching {
+            // 获取包名
+            val packageName: String = context.packageName
+            val resources = context.resources
+            // 获取 R.string 对应的资源 ID
+            val resId: Int = resources.getIdentifier(id, "string", packageName)
+            // 获取字符串
+            if (resId != 0) {
+                return resources.getString(resId)
+            } else {
+                return ""
+            }
+        }.onFailure {
+            return ""
+        }
         return ""
     }
 }
