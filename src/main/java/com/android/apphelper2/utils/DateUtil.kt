9ec60792c3c81d7a82d5225e58a6ee01906c5307
xjxliuxing <xjxliuxing@163.com>
2023-05-12 13:34:12 +0800

    update  date

--- src/main/java/com/android/apphelper2/utils/DateUtil.kt
+++ src/main/java/com/android/apphelper2/utils/DateUtil.kt
@@ -3,7 +3,7 @@ package com.android.apphelper2.utils
 import java.text.SimpleDateFormat
 import java.util.*
 
-object DataUtil {
+object DateUtil {
 
     const val YYYY_MM_DD_HH_MM_SS = "yyyy_MM_dd_HH_mm_ss"
 
