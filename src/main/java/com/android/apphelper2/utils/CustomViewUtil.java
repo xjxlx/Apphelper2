97119280902f71257b1bca82be1cba15310bef5f
xjxliuxing <xjxliuxing@163.com>
2023-07-17 18:37:21 +0800

    update code

---
+++ src/main/java/com/android/apphelper2/utils/CustomViewUtil.java
@@ -0,0 +1,112 @@
+package com.android.apphelper2.utils;
+
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.text.TextUtils;
+
+public class CustomViewUtil {
+
+    /**
+     * @param paint   画笔
+     * @param content 内容
+     * @return 根据画笔和内容返回baseLine的基线, 适用于view写在开始的位置
+     */
+    public static float getBaseLine(Paint paint, String content) {
+        if (paint == null || (TextUtils.isEmpty(content))) {
+            return 0f;
+        }
+        Rect rect = new Rect();
+        paint.getTextBounds(content, 0, content.length(), rect);
+        return (float) Math.abs(rect.top);
+    }
+
+    /**
+     * @param paint   画笔
+     * @param content 文字内容
+     * @return 根据画笔和文字去获取text的宽高  【0】：宽  【1】：高
+     */
+    public static float[] getTextSize(Paint paint, String content) {
+        if (paint == null || (TextUtils.isEmpty(content))) {
+            return null;
+        }
+
+        float[] ints = new float[2];
+        Rect rect = new Rect();
+        paint.getTextBounds(content, 0, content.length(), rect);
+        ints[0] = rect.width();
+        ints[1] = rect.height();
+        return ints;
+    }
+
+    /**
+     * @param paint   画笔
+     * @param content 内容
+     * @return 返回测量文字的宽度
+     */
+    public static float getTextViewWidth(Paint paint, String content) {
+        if (paint == null || TextUtils.isEmpty(content)) {
+            return 0;
+        }
+        return paint.measureText(content, 0, content.length());
+    }
+
+    /**
+     * @param paint   画笔
+     * @param content 内容
+     * @return 获取文字的高度
+     */
+    public static float getTextHeight(Paint paint, String content) {
+        if ((paint != null) && (!TextUtils.isEmpty(content))) {
+            Rect rect = new Rect();
+            paint.getTextBounds(content, 0, content.length(), rect);
+            return rect.height();
+        }
+        return 0;
+    }
+
+    /**
+     * @param paint   画笔
+     * @param content 内容
+     * @return 获取文字的宽度
+     */
+    public static float getTextWidth(Paint paint, String content) {
+        if ((paint != null) && (!TextUtils.isEmpty(content))) {
+            Rect rect = new Rect();
+            paint.getTextBounds(content, 0, content.length(), rect);
+            return rect.width();
+        }
+        return 0;
+    }
+
+    /**
+     * @param paint   画笔
+     * @param rect    写入位置的大小
+     * @param content 内容
+     * @return 返回一个居中显示的baseLine ,适用于在一个区域中显示在最中心的情况
+     */
+    public static float getBaseLienCenter(Paint paint, Rect rect, String content) {
+        if ((paint != null) && (!TextUtils.isEmpty(content)) && rect != null) {
+            //计算baseline
+            Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+            float distance = (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
+            return rect.centerY() + distance;
+        }
+        return 0;
+    }
+
+    /***
+     *
+     * @param paint 画笔
+     * @return 返回drawText 的基线位置
+     */
+    public static float getBaseLine2(Paint paint, Rect rect) {
+        /*
+         * 距离 = 文字高度的一半 - 基线到文字底部的距离（也就是bottom）
+         */
+        Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+        float baseLine = (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
+
+        return baseLine + rect.centerY();
+    }
+
+}
--- src/main/java/com/android/apphelper2/widget/ChartView.kt
+++ src/main/java/com/android/apphelper2/widget/ChartView.kt
@@ -1,21 +1,28 @@
 package com.android.apphelper2.widget
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.android.apphelper2.utils.CustomViewUtil
 import com.android.apphelper2.utils.ResourcesUtil
 
 class ChartView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
 
-    private var mMaxWidth = 500
-    private var mMaxHeight = 322
-
-    private val mBackgroundAngle = 20F
-    private val mBackgroundRectF: RectF = RectF(0F, 0f, mMaxWidth.toFloat(), mMaxHeight.toFloat())
+    private val mMaxWidth: Int by lazy {
+        return@lazy ResourcesUtil.toPx(500F)
+            .toInt()
+    }
+    private val mMaxHeight: Int by lazy {
+        return@lazy ResourcesUtil.toPx(322F)
+            .toInt()
+    }
+    private val mBackgroundAngle: Float by lazy {
+        return@lazy ResourcesUtil.toPx(20F)
+    }
+    private val mBackgroundRectF: RectF by lazy {
+        return@lazy RectF(0F, 0f, mMaxWidth.toFloat(), mMaxHeight.toFloat())
+    }
     private val mPaintBackground: Paint by lazy {
         return@lazy Paint().apply {
             color = Color.parseColor("#33000000")
@@ -23,13 +30,46 @@ class ChartView(context: Context, attributeSet: AttributeSet) : View(context, at
         }
     }
     private val mTitle = "各项提升"
-    private val mTitleLeft = 24F
+    private val mPadding: Float by lazy {
+        return@lazy ResourcesUtil.toPx(24F)
+    }
     private val mPaintTitle: Paint by lazy {
         return@lazy Paint().apply {
             color = Color.WHITE
             style = Paint.Style.FILL
             textSize = ResourcesUtil.toPx(24F)
         }
+    }
+    private val mBottomLine: Float by lazy {
+        return@lazy ResourcesUtil.toPx(50F)
+    }
+
+    // todo 默认的1dp
+    private val mLinesHeight: Float by lazy {
+        return@lazy ResourcesUtil.toPx(1F)
+    }
+    private val mPathEffect: PathEffect by lazy {
+        return@lazy DashPathEffect(floatArrayOf(15F, 15F), -1F)
+    }
+    private val mPaintLines: Paint by lazy {
+        return@lazy Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.parseColor("#33FFFFFF")
+            strokeWidth = mLinesHeight
+            strokeCap = Paint.Cap.ROUND
+        }
+    }
+    private val mLineFirstColor: Int by lazy {
+        return@lazy Color.parseColor("#1AFFFFFF")
+    }
+    private val mLineSecondColor: Int by lazy {
+        return@lazy Color.parseColor("#26FFFFFF")
+    }
+    private val mLineThreadColor: Int by lazy {
+        return@lazy Color.parseColor("#40FFFFFF")
+    }
+    private val mLinesInterval: Float by lazy {
+        return@lazy ResourcesUtil.toPx(60f)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -45,7 +85,32 @@ class ChartView(context: Context, attributeSet: AttributeSet) : View(context, at
             it.drawRoundRect(mBackgroundRectF, mBackgroundAngle, mBackgroundAngle, mPaintBackground)
 
             // 2: draw top text
-            it.drawText(mTitle, mTitleLeft, mTitleLeft, mPaintTitle)
+            val mTitleBaseLine = CustomViewUtil.getBaseLine(mPaintTitle, mTitle)
+            it.drawText(mTitle, mPadding, mTitleBaseLine + mPadding, mPaintTitle)
+
+            // 3：draw bottom line
+            val mBottomLineLocation = mMaxHeight - mBottomLine
+            val mLineLeft = mPadding
+            val mLineRight = mMaxWidth - mPadding
+
+            it.drawLine(mLineLeft, mBottomLineLocation, mLineRight, mBottomLineLocation, mPaintLines)
+
+            mPaintLines.pathEffect = mPathEffect
+
+            // 3.1 draw first line
+            val firstLineLocation = mBottomLineLocation - mLinesInterval
+            mPaintLines.color = mLineFirstColor
+            it.drawLine(mLineLeft, firstLineLocation, mLineRight, firstLineLocation, mPaintLines)
+
+            // 3.2 draw second line
+            val secondLineLocation = firstLineLocation - mLinesInterval
+            mPaintLines.color = mLineSecondColor
+            it.drawLine(mLineLeft, secondLineLocation, mLineRight, secondLineLocation, mPaintLines)
+
+            // 3.3 draw three line
+            val threeLineLocation = secondLineLocation - mLinesInterval
+            mPaintLines.color = mLineThreadColor
+            it.drawLine(mLineLeft, threeLineLocation, mLineRight, threeLineLocation, mPaintLines)
 
         }
     }
