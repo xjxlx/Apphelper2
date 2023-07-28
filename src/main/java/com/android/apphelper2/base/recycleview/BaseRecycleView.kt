abe19f892dbb91e86d1ad68a0bff681b2e9617e9
xjxliuxing <xjxliuxing@163.com>
2023-07-29 00:30:38 +0800

    update utils

--- src/main/java/com/android/apphelper2/base/recycleview/BaseRecycleView.kt
+++ src/main/java/com/android/apphelper2/base/recycleview/BaseRecycleView.kt
@@ -1,5 +1,6 @@
 package com.android.apphelper2.base.recycleview
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +9,11 @@ import androidx.recyclerview.widget.RecyclerView
 abstract class BaseRecycleView<T, E : BaseVH> : RecyclerView.Adapter<E>() {
 
     var mList: MutableList<T> = mutableListOf()
+    var mContext: Context? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): E {
+        mContext = parent.context
+
         val resource = getLayout(viewType)
         val view = LayoutInflater.from(parent.context)
             .inflate(resource, parent, false)
