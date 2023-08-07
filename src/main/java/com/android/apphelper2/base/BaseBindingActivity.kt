4e364887c5afa2ddb3754bd1dfb5f33da9721fd8
xjxliuxing <xjxliuxing@163.com>
2023-08-07 12:16:20 +0800

    update utils

--- src/main/java/com/android/apphelper2/base/BaseBindingActivity.kt
+++ src/main/java/com/android/apphelper2/base/BaseBindingActivity.kt
@@ -51,6 +51,7 @@ open abstract class BaseBindingActivity<T : ViewBinding> : AppCompatActivity(), 
             StatusBarUtil.getInstance(this)
                 .setStatusFontColor(true)
                 .setStatusColor(statusBar)
+                .setStatusFontColor(true)
         }
     }
 
