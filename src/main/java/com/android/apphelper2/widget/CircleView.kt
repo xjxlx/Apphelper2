45c3996e4fd773eae4b584be370004280f70e497
xjxliuxing <xjxliuxing@163.com>
2023-08-10 00:16:00 +0800

    update main

---
+++ src/main/java/com/android/apphelper2/widget/CircleView.kt
@@ -0,0 +1,8 @@
+package com.android.apphelper2.widget
+
+import android.content.Context
+import android.util.AttributeSet
+
+class CircleView(context: Context, attributeSet: AttributeSet) : androidx.appcompat.widget.AppCompatImageView(context, attributeSet) {
+
+}
