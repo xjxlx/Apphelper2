738faed7e25612322b3d4da1cdaa578121373318
xjxliuxing <xjxliuxing@163.com>
2023-08-11 18:28:13 +0800

    update utils

--- src/main/java/com/android/apphelper2/widget/CircleView.kt
+++ src/main/java/com/android/apphelper2/widget/CircleView.kt
@@ -2,36 +2,62 @@ package com.android.apphelper2.widget
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.TypedArray
 import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
+import com.android.apphelper2.R
 import com.android.common.utils.BitmapUtil
 import com.android.common.utils.LogUtil
-import com.android.common.utils.ResourcesUtil
 
 class CircleView(context: Context, attributeSet: AttributeSet) : androidx.appcompat.widget.AppCompatImageView(context, attributeSet) {
 
     private val mPath: Path by lazy {
         return@lazy Path()
     }
-    private val mPaint: Paint by lazy {
+    private val mPaintBitmap: Paint by lazy {
         return@lazy Paint(Paint.ANTI_ALIAS_FLAG).apply {
             style = Paint.Style.FILL
-            color = Color.RED
-            strokeWidth = ResourcesUtil.getDimension(context, com.apphelper.demens.R.dimen.dp_2)
         }
     }
-    private val mSrc = Rect()
-    private val mDes = Rect()
+    private val mBitmapSrc = Rect()
+    private val mBitmapDes = Rect()
+    private val mPaintStroke: Paint by lazy {
+        return@lazy Paint().also {
+            it.style = Paint.Style.STROKE
+        }
+    }
+
+    init {
+        val typedArray: TypedArray = context.obtainStyledAttributes(attributeSet, R.styleable.CircleView)
+        typedArray.getColor(R.styleable.CircleView_stroke_color, 0)
+            .also {
+                if (it != 0) {
+                    mPaintStroke.color = it
+                }
+            }
+        typedArray.getDimension(R.styleable.CircleView_stroke_width, 0F)
+            .also {
+                if (it != 0F) {
+                    mPaintStroke.strokeWidth = it
+                }
+            }
+        typedArray.recycle()
+    }
 
     @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas?) {
         // 1：避免重复性绘制
         // super.onDraw(canvas)
 
+        val strokeWidth = mPaintStroke.strokeWidth
+
         if (width > 0 && drawable != null && canvas != null) {
             //  既然是绘制圆形，那么宽和高必须是相同的，这里取宽的高度，或者高的高度，都是一样的，这里就直接取值宽的高度了
-            val radius = (width / 2).toFloat()
+            val targetWidth = width - (strokeWidth * 2).toInt()
+            val targetHeight = height - (strokeWidth * 2).toInt()
+
+            val radius = (targetWidth / 2).toFloat()
             if (radius > 0) {
                 // 2：保存当前的状态
                 canvas.save()
@@ -43,7 +69,7 @@ class CircleView(context: Context, attributeSet: AttributeSet) : androidx.appcom
                 if (drawable is BitmapDrawable) {
                     val bitmap = (drawable as BitmapDrawable).bitmap
                     // 5: 缩放图片的比例，重新生成一个bitmap，避免图片过大，只能取值到一部分图片
-                    val scaleBitmap = BitmapUtil.getScaleBitmap(bitmap, width, height)
+                    val scaleBitmap = BitmapUtil.getScaleBitmap(bitmap, targetWidth, targetHeight)
                     if (scaleBitmap == null) {
                         LogUtil.e("scaleBitmap is null !")
                         return
@@ -52,40 +78,59 @@ class CircleView(context: Context, attributeSet: AttributeSet) : androidx.appcom
                     val bitmapWidth = scaleBitmap.width
                     val bitmapHeight = scaleBitmap.height
 
-                    if (bitmapWidth > width) {
+                    if (bitmapWidth > targetWidth) {
                         // left = (bitmap.width - view.width) / 2, this.value > 0
-                        mSrc.left = (bitmapWidth - width) / 2
-                    } else if (bitmapWidth == width) {
-                        mSrc.left = 0
+                        mBitmapSrc.left = (bitmapWidth - targetWidth) / 2
+                    } else if (bitmapWidth == targetWidth) {
+                        mBitmapSrc.left = 0
                     } else {
                         // (view.width - bitmap.with) / 2, this.value > 0
-                        mSrc.left = 0
+                        mBitmapSrc.left = 0
                     }
 
-                    if (bitmapHeight > height) {
+                    if (bitmapHeight > targetHeight) {
                         // top = (bitmap.top - view.top)/ 2
-                        mSrc.top = (bitmapHeight - height) / 2
-                    } else if (bitmapHeight == height) {
-                        mSrc.top = 0
+                        mBitmapSrc.top = (bitmapHeight - targetHeight) / 2
+                    } else if (bitmapHeight == targetHeight) {
+                        mBitmapSrc.top = 0
                     } else {
                         // (view.height - bitmap.height) / 2
-                        mSrc.top = (height - bitmapHeight) / 2
+                        mBitmapSrc.top = (targetHeight - bitmapHeight) / 2
                     }
 
-                    mSrc.right = mSrc.left + width
-                    mSrc.bottom = mSrc.top + height
+                    mBitmapSrc.right = mBitmapSrc.left + targetWidth
+                    mBitmapSrc.bottom = mBitmapSrc.top + targetHeight
 
-                    mDes.left = 0
-                    mDes.top = 0
-                    mDes.right = mDes.left + width
-                    mDes.bottom = mDes.top + height
+//                    mBitmapDes.left = if (strokeWidth > 0) {
+//                        strokeWidth
+//                    } else {
+//                        0
+//                    }
+//                    mBitmapDes.top = if (strokeWidth > 0) {
+//                        strokeWidth
+//                    } else {
+//                        0
+//                    }
+                    mBitmapDes.right = mBitmapDes.left + targetWidth
+                    mBitmapDes.bottom = mBitmapDes.top + targetHeight
 
                     // 6：绘制drawable
-                    canvas.drawBitmap(scaleBitmap, mSrc, mDes, mPaint)
+//                    canvas.drawBitmap(scaleBitmap, mBitmapSrc, mBitmapDes, mPaintBitmap)
                 }
 
                 // 7：还原
                 canvas.restore()
+            }
+        }
+
+        // 8: 绘制描边
+        canvas?.let {
+            if (strokeWidth > 0) {
+                val circleX = (width / 2).toFloat()
+                val circleY = (width / 2).toFloat()
+                val radius = ((width - (strokeWidth * 2)) / 2)
+
+                it.drawCircle(circleX, circleY, radius, mPaintStroke)
             }
         }
     }
