534dd80ca2371a385f71ef2984631e60216814d1
xjxliuxing@163.com <xjxliuxing@163.com>
2023-11-08 01:14:10 +0800

    update files

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 }
 
 android {
+    namespace = "com.android.apphelper2"
     compileSdk = libs.versions.compileSdks.get().toInt()
     defaultConfig {
         minSdk = libs.versions.minSdk.get().toInt()
